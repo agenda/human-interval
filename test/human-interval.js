944ea471d33d2b4bb127648f47364c7890e1b25f
CapacitorSet <CapacitorSet@users.noreply.github.com>
2015-07-26 21:37:20 +0200

    Added support for the inverse operation (converting machine timestamps to human-readable time intervals)

--- test/human-interval.js
+++ test/human-interval.js
@@ -3,69 +3,116 @@ var expect = require('expect.js'),
 
 describe('Human Interval', function() {
   it("returns the number when given a number", function() {
-    expect(humanInterval(5000)).to.be(5000);
+    expect(humanInterval.human(5000)).to.be(5000);
   });
 
   it("returns undefined when given undefined", function() {
-    expect(humanInterval(undefined)).to.be(undefined);
+    expect(humanInterval.human(undefined)).to.be(undefined);
   });
 
   it('does not require a number', function() {
-      expect(humanInterval('week')).to.be(7 * 86400000);
+      expect(humanInterval.human('week')).to.be(7 * 86400000);
   });
 
   describe('basic units', function() {
     it('understands seconds', function() {
-      expect(humanInterval('1 second')).to.be(1000);
+      expect(humanInterval.human('1 second')).to.be(1000);
     });
     it('understands minutes', function() {
-      expect(humanInterval('1 minute')).to.be(60000);
+      expect(humanInterval.human('1 minute')).to.be(60000);
     });
     it('understands hours', function() {
-      expect(humanInterval('1 hour')).to.be(3600000);
+      expect(humanInterval.human('1 hour')).to.be(3600000);
     });
     it('understands days', function() {
-      expect(humanInterval('1 day')).to.be(86400000);
+      expect(humanInterval.human('1 day')).to.be(86400000);
     });
     it('understands weeks', function() {
-      expect(humanInterval('1 week')).to.be(7 * 86400000);
+      expect(humanInterval.human('1 week')).to.be(7 * 86400000);
     });
     it('understands months', function() {
-      expect(humanInterval('1 month')).to.be(30 * 86400000);
+      expect(humanInterval.human('1 month')).to.be(30 * 86400000);
     });
     it('understands years', function() {
-      expect(humanInterval('1 year')).to.be(31536000000);
+      expect(humanInterval.human('1 year')).to.be(31536000000);
     });
   });
 
   describe("basic numbers", function() {
     it("understands numbers", function() {
-      expect(humanInterval('2 seconds')).to.be(2000);
+      expect(humanInterval.human('2 seconds')).to.be(2000);
     });
 
     it("understands decimals", function() {
-      expect(humanInterval('2.5 seconds')).to.be(2500);
+      expect(humanInterval.human('2.5 seconds')).to.be(2500);
     });
   });
 
   describe("english numbers", function() {
     it("understands numbers", function() {
-      expect(humanInterval("two seconds")).to.be(2000);
+      expect(humanInterval.human("two seconds")).to.be(2000);
     });
   });
 
   describe("mixes", function() {
     it("works with long numbers", function() {
-      expect(humanInterval('3 minutes and 30 seconds')).to.be(210000);
+      expect(humanInterval.human('3 minutes and 30 seconds')).to.be(210000);
     });
 
     it("works with mixed units", function() {
-      expect(humanInterval('3 minutes and 30 seconds')).to.be(210000);
+      expect(humanInterval.human('3 minutes and 30 seconds')).to.be(210000);
     });
 
     it("works with mixed time expressions", function() {
-      expect(humanInterval('three minutes and 30 seconds')).to.be(210000);
-      expect(humanInterval('three minutes 30 seconds')).to.be(210000);
+      expect(humanInterval.human('three minutes and 30 seconds')).to.be(210000);
+      expect(humanInterval.human('three minutes 30 seconds')).to.be(210000);
+    });
+  });
+});
+
+
+describe('Machine Interval', function() {
+
+  it("returns undefined when given undefined", function() {
+    expect(humanInterval.machine(undefined)).to.be(undefined);
+  });
+
+  describe('basic units', function() {
+    it('outputs seconds', function() {
+      expect(humanInterval.machine(1000)).to.be('one second');
+    });
+    it('outputs minutes', function() {
+      expect(humanInterval.machine(60000)).to.be('one minute');
+    });
+    it('outputs hours', function() {
+      expect(humanInterval.machine(3600000)).to.be('one hour');
+    });
+    it('outputs days', function() {
+      expect(humanInterval.machine(86400000)).to.be('one day');
+    });
+    it('outputs weeks', function() {
+      expect(humanInterval.machine(7 * 86400000)).to.be('one week');
+    });
+    it('outputs months', function() {
+      expect(humanInterval.machine(30 * 86400000)).to.be('one month');
+    });
+    it('outputs years', function() {
+      expect(humanInterval.machine(31536000000)).to.be('one year');
+    });
+  });
+
+  describe("basic numbers", function() {
+    it("outputs numbers", function() {
+      expect(humanInterval.machine(2000)).to.be('two seconds');
+      expect(humanInterval.machine(
+        10*humanInterval.units.year  +
+        9*humanInterval.units.month +
+        3*humanInterval.units.week  +
+        2*humanInterval.units.day   +
+        8*humanInterval.units.hour  +
+        7*humanInterval.units.minute+
+        5*humanInterval.units.second
+      )).to.be('ten years, nine months, three weeks, two days, eight hours, seven minutes, five seconds');
     });
   });
 });
