8884a3cb57a9c167bd8c0e3a2b9de70894cf3765
Mikael Korpela <mikael@ihminen.org>
2017-10-29 03:46:33 +0200

    Expand tests
    - Cover more invalid values
    - Cover every time unit for singluar/plural/decimals
    - Cover every english number (1-10)
    - Use object to pick right unit values

--- test.js
+++ test.js
@@ -6,18 +6,106 @@ const macro = (t, inputs, expected) => {
   inputs.forEach(input => t.is(humanInterval(input), expected));
 };
 
-test('returns the number when given a number', macro, 5000, 5000);
-test('returns undefined when given undefined', macro, undefined, undefined);
-test('does not require a number', macro, 'week', 7 * 86400000);
-test('understands seconds', macro, '1 second', 1000);
-test('understands minutes', macro, '1 minute', 60000);
-test('understands hours', macro, '1 hour', 3600000);
-test('understands days', macro, '1 day', 86400000);
-test('understands weeks', macro, '1 week', 7 * 86400000);
-test('understands months', macro, '1 month', 30 * 86400000);
-test('understands years', macro, '1 year', 31536000000);
-test('understands numbers', macro, '2 seconds', 2000);
-test('understands decimals', macro, '2.5 seconds', 2500);
-test('understands english numbers', macro, 'two seconds', 2000);
-test('works with mixed units', macro, '3 minutes and 30 seconds', 210000);
-test('works with mixed time expressions', macro, ['three minutes and 30 seconds', 'three minutes 30 seconds'], 210000);
+const units = {};
+units.second = 1000;
+units.minute = units.second * 60;
+units.hour = units.minute * 60;
+units.day = units.hour * 24;
+units.week = units.day * 7;
+units.month = units.day * 30;
+units.year = units.day * 365;
+
+const timeUnits = [
+  'second',
+  'minute',
+  'hour',
+  'day',
+  'week',
+  'month',
+  'year'
+];
+
+const englishNumbers = [
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+  'ten'
+];
+
+// Invalid values:
+test('Returns undefined when given undefined', macro, undefined, undefined);
+test('Returns null when given null', macro, null, null);
+test('Returns empty string when given empty string', macro, '', '');
+test('Returns NaN when given unknown string', macro, 'foobar', NaN);
+
+// Single values
+test('Returns the number when given a number', macro, 5000, 5000);
+test('Understands numbers', macro, '2 seconds', 2 * units.second);
+test('Understands decimals', macro, '2.5 seconds', 2.5 * units.second);
+test('Understands long decimals', macro, '2.5555 seconds', 2.5555 * units.second);
+
+// Understands time base units without numbers
+// i.e. "second === 1000"
+timeUnits.forEach(unit => {
+  test(
+    'Understands "' + unit + '" without number',
+    macro,
+    unit,
+    Number(units[unit])
+  );
+});
+
+// Understands time base units in singular
+// i.e. "1 second === 1000"
+timeUnits.forEach(unit => {
+  test(
+    'Understands "1 ' + unit + '"',
+    macro,
+    '1 ' + unit,
+    Number(units[unit])
+  );
+});
+
+// Understands time base units in plural
+// i.e. "2 seconds === 2000"
+timeUnits.forEach(unit => {
+  test(
+    'Understands ' + unit + 's in plural',
+    macro,
+    '2 ' + unit + 's',
+    2 * Number(units[unit])
+  );
+});
+
+// Understands time base units in decimals values
+// i.e. "1.5 seconds === 1500"
+timeUnits.forEach(unit => {
+  test(
+    'Understands ' + unit + 's in decimals',
+    macro,
+    '1.5 ' + unit + 's',
+    1.5 * Number(units[unit])
+  );
+});
+
+// English numbers
+// i.e. "one second === 1000"
+englishNumbers.forEach((num, i) => {
+  test(
+    'Understands english number ' + num,
+    macro,
+    num + ' seconds',
+    (i + 1) * units.second
+  );
+});
+
+// Mixed
+test('Works with mixed units in plural units', macro, ['3 minutes and 30 seconds', '3 minutes 30 seconds'], (3 * units.minute) + (30 * units.second));
+test('Works with mixed units in singular units', macro, ['1 minute and 1 second', '1 minute 1 second'], units.minute + units.second);
+test('Works with mixed time expressions', macro, ['three minutes and 30 seconds', 'three minutes 30 seconds'], (3 * units.minute) + (30 * units.second));
