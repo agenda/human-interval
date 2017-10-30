95f14cec6eeff82c8601f27e29f7b3322ea9b4d1
Farid Neshat <FaridN_SOAD@yahoo.com>
2017-10-31 03:15:36 +0800

    Refactor module to use node-numbered instead

--- test.js
+++ test.js
@@ -42,7 +42,7 @@ const englishNumbers = [
 test('Returns undefined when given undefined', macro, undefined, undefined);
 test('Returns null when given null', macro, null, null);
 test('Returns empty string when given empty string', macro, '', '');
-test('Returns NaN when given unknown string', macro, 'foobar', Number.NaN);
+test('Returns NaN when given unknown string', macro, 'foobar', NaN);
 
 // Single values
 test('Returns the number when given a number', macro, 5000, 5000);
@@ -96,16 +96,30 @@ timeUnits.forEach(unit => {
 
 // English numbers
 // i.e. "one second === 1000"
-englishNumbers.forEach((number, i) => {
+englishNumbers.forEach((num, i) => {
   test(
-    'Understands english number ' + number,
+    'Understands english number ' + num,
     macro,
-    number + ' seconds',
+    num + ' seconds',
     (i + 1) * units.second
   );
 });
 
+// Understands negative numbers
+// i.e. "-2 seconds === -2000"
+timeUnits.forEach(unit => {
+  test(
+    'Understands negative ' + unit + 's',
+    macro,
+    '-2 ' + unit + 's',
+    -2 * Number(units[unit])
+  );
+});
+
 // Mixed
-test('Works with mixed units in plural units', macro, ['3 minutes and 30 seconds', '3 minutes 30 seconds'], (3 * units.minute) + (30 * units.second));
-test('Works with mixed units in singular units', macro, ['1 minute and 1 second', '1 minute 1 second'], units.minute + units.second);
-test('Works with mixed time expressions', macro, ['three minutes and 30 seconds', 'three minutes 30 seconds'], (3 * units.minute) + (30 * units.second));
+test('Understands mixed units', macro, '3 minutes and 30 seconds', (3 * units.minute) + (30 * units.second));
+test('Understands mixed time expressions with plurals', macro, ['three minutes and 30 seconds', 'three minutes 30 seconds'], (3 * units.minute) + (30 * units.second));
+test('Understands mixed time expressions with singulars', macro, ['one minute and 1 second', 'one minute 1 second'], units.minute + units.second);
+test('Understands 2 digit english numbers', macro, 'thirty three seconds', 33 * units.second);
+test('Understands mix units + multi digit english numbers', macro, 'hundred and three seconds and twelve minutes', (103 * units.second) + (12 * units.minute));
+test('Understands hyphenated numbers', macro, 'three hundred and twenty-five seconds', (325 * units.second));
