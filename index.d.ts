ed1c53691274682e96acae2b4b9434106b37e1c2
Mikael Korpela <mikael@ihminen.org>
2021-01-17 01:19:42 +0200

    Remove namespace

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,3 @@
 declare function humanInterval(time?: string): number | undefined;
 
-declare namespace humanInterval {
-  type LanguageMap = Record<string, number>;
-
-  let languageMap: LanguageMap;
-}
-
 export = humanInterval;
